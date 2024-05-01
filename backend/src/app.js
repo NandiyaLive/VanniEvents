2d74d510f7798d250d1eeb182cf06b43e26d541c
Neranjana Prasad <neranjanaprsd@gmail.com>
2024-05-01 08:09:02 +0530

    feat: project setup

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import { PORT } from "@/utils/config";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
