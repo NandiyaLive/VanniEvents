4d661162711046536cd6d1c8231b712ac544ea2a
Neranjana Prasad <neranjanaprsd@gmail.com>
2024-06-26 17:56:35 +0530

    feat: add dist to repo

---
+++ backend/dist/models/club.js
@@ -0,0 +1,44 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _require = require("mongoose"),
+  Schema = _require.Schema,
+  model = _require.model;
+var ClubSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String
+  },
+  logo: {
+    type: String
+  },
+  cover: {
+    type: String
+  },
+  admins: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  events: [{
+    type: Schema.Types.ObjectId,
+    ref: "Event"
+  }]
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+var Club = model("Club", ClubSchema);
+var _default = exports["default"] = Club;
